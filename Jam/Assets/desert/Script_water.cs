681d30a1b8e81368e74dced3933293064d6b7725
COUSINThibaut <146203877+COUSINThibaut@users.noreply.github.com>
2025-02-08 17:52:08 +0100

    [+] Add first oasis

---
+++ Jam/Assets/desert/Script_water.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Water_level : MonoBehaviour
+{
+    public Transform player;
+    public float fadeDistance = 20f;
+    public float maxFadeDistance = 60f;
+    private float initialY;
+
+    void Start()
+    {
+        initialY = transform.position.y;
+    }
+
+    void Update()
+    {
+        
+        float distance = Vector3.Distance(player.position, transform.position);
+        float alpha = Mathf.InverseLerp(fadeDistance, maxFadeDistance, distance);
+        float newY = Mathf.Lerp(initialY, initialY - 10f, alpha);
+      
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
