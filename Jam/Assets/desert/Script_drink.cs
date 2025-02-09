a61d533bf49af252f368548a90412c7e0699ff89
COUSINThibaut <146203877+COUSINThibaut@users.noreply.github.com>
2025-02-09 11:02:47 +0100

    [+] adding wall around the map and fixing the render distance

--- Jam/Assets/desert/Script_drink.cs
+++ Jam/Assets/desert/Script_drink.cs
@@ -1,25 +1,22 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;  // Nécessaire pour charger des scènes
-using UnityEngine.UI;  // Nécessaire pour manipuler l'UI
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Script_drink : MonoBehaviour
 {
-    public GameObject messageUI;  // Le GameObject contenant le texte du message "E pour boire"
-    private bool playerInRange = false;  // Pour vérifier si le joueur est dans la zone du trigger
+    public GameObject messageUI;
+    private bool playerInRange = false;
 
     void Start()
     {
-        // Masquer le message au début (avant que le joueur n'entre dans la zone)
         messageUI.SetActive(false);
     }
 
     void Update()
     {
-        // Si le joueur est dans la zone et qu'il appuie sur 'E'
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
             Debug.Log("E cliked");
-            // Charger la scène suivante (remplacer "SceneName" par le nom de la scène suivante)
             SceneManager.LoadScene("Labyrinth");
         }
     }
